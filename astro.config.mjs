0ce4ea3fd909ed1af31ec5ebf9574539d4e6e7f5
Rafix <rafal987.1@o2.pl>
2026-08-09 19:10:38 +0200

    chore: add properties to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
+  devToolbar: {
+    enabled: false,
+  },
   i18n: {
     defaultLocale: 'pl',
     locales: ['pl', 'en'],
@@ -17,7 +20,13 @@ export default defineConfig({
     server: {
       watch: {
         usePolling: true,
+        interval: 100,
+        ignored: ['!**/src/**/*.{astro,js,ts,jsx,tsx}'],
       },
+    },
+    optimizeDeps: {
+      exclude: ['@tailwindcss/vite'],
+      force: true,
     },
   },
 });
